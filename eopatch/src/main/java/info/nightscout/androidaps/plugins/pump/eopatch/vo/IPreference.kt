82b7a62a3db85e3f06527039e104efb30339e219
BernhardRo <35614076+BernhardRo@users.noreply.github.com>
2022-10-01 20:18:08 +0200

    back to AAPS master
    back to AAPS master

--- eopatch/src/main/java/info/nightscout/androidaps/plugins/pump/eopatch/vo/IPreference.kt
+++
@@ -1,9 +0,0 @@
-package info.nightscout.androidaps.plugins.pump.eopatch.vo
-
-import info.nightscout.shared.sharedPreferences.SP
-import io.reactivex.Observable
-
-interface IPreference<T>{
-    fun flush(sp: SP)
-    fun observe(): Observable<T>
-}
